ad97f1a1acc4b70b5f74cd1951c642ec7fb8db7d
lhqing <liuhq@ucsd.edu>
2020-06-26 17:33:02 -0700

    add app name

--- omb/app.py
+++ omb/app.py
@@ -4,8 +4,8 @@ Main Dash apps
 
 import dash
 
-external_stylesheets = ['https://raw.githubusercontent.com/lhqing/omb/master/omb/assets/s1.css',
-                        'https://raw.githubusercontent.com/lhqing/omb/master/omb/assets/styles.css']
+#external_stylesheets = ['https://raw.githubusercontent.com/lhqing/omb/master/omb/assets/s1.css',
+#                        'https://raw.githubusercontent.com/lhqing/omb/master/omb/assets/styles.css']
 
 app = dash.Dash(
     __name__,
@@ -15,7 +15,7 @@ app = dash.Dash(
             "content": "width=device-width"
         }
     ],
-    external_stylesheets=external_stylesheets
+    #external_stylesheets=external_stylesheets
 )
 
 server = app.server
@@ -23,12 +23,12 @@ app.config.suppress_callback_exceptions = True
 
 # this is only based on my own server
 # server deploy setting, change APP_NAME based on the routing config in apache
-# APP_NAME = 'omb'
-# app.config.update({
-#     # remove the default of '/'
-#     'routes_pathname_prefix': f'/{APP_NAME}/',
-#
-#     # remove the default of '/'
-#     'requests_pathname_prefix': f'/{APP_NAME}/'
-# })
+APP_NAME = 'omb'
+app.config.update({
+    # remove the default of '/'
+    'routes_pathname_prefix': f'/{APP_NAME}/',
+
+    # remove the default of '/'
+    'requests_pathname_prefix': f'/{APP_NAME}/'
+})
 
