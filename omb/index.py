0c7a0ec805ff96392c265c08ff69934972dd8d2e
lhqing <liuhq@ucsd.edu>
2020-06-26 17:12:39 -0700

    Update index.py

--- omb/index.py
+++ omb/index.py
@@ -5,9 +5,9 @@ Main app entry point and routing control
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from .backend import dataset
-from .app import app, server
-from .apps import *
+from omb.backend import dataset
+from omb.app import app, server
+from omb.apps import *
 
 # make sure pycharm do not remove the import line...
 type(server)
