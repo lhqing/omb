ae5bb318b088bb21a612e0784c4eafeed7224b24
lhqing <liuhq@ucsd.edu>
2020-07-04 11:20:41 -0700

    update index for deploy

--- omb/app.py
+++ omb/app.py
@@ -8,7 +8,6 @@ APP_ROOT_NAME = 'omb'
 
 app = dash.Dash(
     __name__,
-    suppress_callback_exceptions=True,
     meta_tags=[
         {
             "name": "viewport",
--- omb/index_for_deploy.py
+++ omb/index_for_deploy.py
@@ -3,8 +3,9 @@ Main app entry point and routing control
 """
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
+
 from omb.app import app, server, APP_ROOT_NAME
 from omb.apps import *
 
@@ -31,28 +32,76 @@ app.layout = html.Div([
 ])
 
 
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname is None:
-        raise PreventUpdate
-    print('Input pathname', pathname)
-    pathname = '/' + pathname.split('/')[-1]
-    print('Used pathname', pathname)
+def search_to_dict(search):
+    if search is None:
+        return None
+    kv_pairs = search[1:].split(';')  # remove ?
+    search_dict = {}
+    for kv in kv_pairs:
+        try:
+            k, v = kv.split('=')
+            search_dict[k] = v
+        except (IndexError, ValueError):
+            return None
+    return search_dict
 
-    if pathname == '/test':
-        return test_app.layout
-    if pathname == '/home':
-        return home_app.layout
-    if pathname == '/brain_table':
-        return brain_table_app_layout
-    if pathname == '/brain_region':
-        return region_browser_app.layout
-    if pathname == '/':
-        return test_app.layout
+
+def get_header():
+    return html.Div(
+        children=[
+            html.Div(children=[
+                dcc.Link(
+                    "Home",
+                    href="/home",
+                    className="tab first",
+                ),
+                dcc.Link(
+                    "Brain Region Browser",
+                    href="/brain_region",
+                    className="tab",
+                ),
+                dcc.Link(
+                    "Cell Type Browser",
+                    href="/cell_type?ct=Exc",
+                    className="tab",
+                )
+            ],
+                className="row all-tabs")
+        ]
+    )
+
+
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname')],
+    [State('url', 'search'),
+     State('url', 'href')]
+)
+def display_page(pathname, search, total_url):
+    # print('url.pathname', pathname)
+    # print('url.search', search)
+    app_layout = get_header()
+    if pathname is None:
+        # init callback url is None
+        raise PreventUpdate
+    elif pathname == '/home':
+        pass
+    elif pathname == '/brain_region':
+        app_layout.children.append(region_browser_app.layout)
+    elif pathname == '/cell_type':
+        search_dict = search_to_dict(search)
+        if search_dict is None:
+            return '404'
+        # validate key here:
+        if 'ct' not in search_dict:
+            return '404'
+        layout = create_cell_type_browser_layout(cell_type_name=search_dict['ct'], total_url=total_url)
+        if layout is None:
+            return '404'
+        else:
+            app_layout.children.append(layout)
+    elif pathname == '/test':
+        app_layout.children.append(test_app.layout)
     else:
         return '404'
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True, port='1234')
+    return app_layout
