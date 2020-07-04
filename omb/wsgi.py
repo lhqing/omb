5568950ab309c000a44f6cc875ef04fe344a3474
lhqing <liuhq@ucsd.edu>
2020-07-04 11:37:57 -0700

    update wsgi

--- omb/wsgi.py
+++ omb/wsgi.py
@@ -3,4 +3,5 @@
 import sys
 sys.path.insert(0, "/var/www/html/omb")
 
-from index import server as application
+# use a different index for deploy
+from index_for_deploy import server as application
